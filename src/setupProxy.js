02e1dd405c8c2cd77c81f20989b73cba9e5daf60
Matthew Demoy <MatthewDemoy@student.uml.edu>
2021-03-10 22:33:36 -0500

    rm yarn

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,8 +6,6 @@ var cors = require('cors');
 const apiUrl = process.env.REACT_APP_API_URL
 
 module.exports = function(app) {
-    console.log(apiUrl)
-    app.use(cors());
     app.use(
         proxy('/api', { target:  process.env.REACT_APP_API_URL })
       );
