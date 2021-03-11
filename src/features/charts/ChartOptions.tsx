4816dd2d44d17ff1a334eb5b02bd7b6106f0fa1b
Matthew Demoy <MatthewDemoy@student.uml.edu>
2021-03-10 22:06:46 -0500

    correct env variable name

--- src/features/charts/ChartOptions.tsx
+++ src/features/charts/ChartOptions.tsx
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { getCharts } from "./list/chartListSlice";
 import { useDispatch} from "react-redux"
 
 const ChartOptions = () => {
     const dispatch = useDispatch()
+
+    const [isPopular, changePopular] = useState<boolean>(true)
+    let popularBtnClasses = ['col-span-3 h-12 my-2']
+    let selectionBtnClasses = ['col-span-3  h-12 my-2"']
+
+    const handlePopularClick = () => {
+      dispatch(getCharts(true))
+      changePopular(true)
+    }
+
+    const handleSelectionClick = () => {
+      dispatch(getCharts(false))
+      changePopular(false)
+    }
+    
   return (
     <div className={"grid grid-cols-12 sm:grid-cols-10"}>
       <span className={"col-span-2 sm:col-span-1"} />
-      <button className={"col-span-3 h-12 my-2"} onClick={()=>{dispatch(getCharts(true))}}>Popular</button>
+      <button className={"col-span-3 h-12 my-2" + (isPopular ? ' bg-gray-400' : '')} onClick={handlePopularClick}>Popular</button>
       <span className={"col-span-2"} >
           
           </span>
-      <button className={"col-span-3  h-12 my-2"} onClick={()=>{dispatch(getCharts(false))}} >Selections</button>
+      <button className={"col-span-3  h-12 my-2" + (!isPopular && ' bg-gray-400')} onClick={handleSelectionClick} >Selections</button>
       <span className={"col-span-2 sm:col-span-1"} />
     </div>
   );
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,12 +3,12 @@ const proxy = require('http-proxy-middleware');
 
 var cors = require('cors');
 
-const apiUrl = process.env.API_URL
+const apiUrl = process.env.REACT_APP_API_URL
 
 module.exports = function(app) {
     console.log(apiUrl)
     app.use(cors());
     app.use(
-        proxy('/api', { target:  apiUrl || 'http://localhost:8080'})
+        proxy('/api', { target:  process.env.REACT_APP_API_URL })
       );
 }
